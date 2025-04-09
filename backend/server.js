2dce2ff52211b100692ef2c465a0761a8a3774d6
Anks2833 <dubeyankur2833@gmail.com>
2025-04-09 20:34:51 +0530

    cors-configured-2

--- backend/server.js
+++ backend/server.js
@@ -70,8 +70,8 @@ app.use(
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://hybrid-realty-dev.vercel.app/",
-      "https://hybrid-realty-dev-admin.vercel.app/",
+      "https://hybrid-realty-dev.vercel.app",
+      "https://hybrid-realty-dev-admin.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
